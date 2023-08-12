a33d917a0679683267431a922b95b603798afce4
Babasaheb Kothawale <kothawaleb@gmail.com>
2023-08-12 21:51:51 +0530

    add ktor dependency

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -26,14 +26,31 @@ kotlin {
     }
 
     sourceSets {
+        val ktorVersion = "2.3.3"
         val commonMain by getting {
             dependencies {
                 //put your multiplatform dependencies here
+                implementation ("io.ktor:ktor-client-core:$ktorVersion")
+                implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                implementation ("com.google.android.material:material:1.9.0")
+                implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
+                implementation ("io.ktor:ktor-client-android:$ktorVersion")
+            }
+        }
+
+        val iosMain by getting {
+            dependencies {
+                implementation ("io.ktor:ktor-client-ios:$ktorVersion")
             }
         }
     }
